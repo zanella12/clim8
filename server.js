079f46d9d5540bf7d54716fcf418d6435d7c18cb
goncalopolido <goncalopolido@gmail.com>
2025-04-16 09:44:52 +0000

    remove comments

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.static('public'));
 
-// API Routes
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
-// Handle 404s
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
